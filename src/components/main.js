fd7e4558015c19a20e989ec898c61ebcbce79995
Kaian Samuel <kaiansamuelster@gmail.com>
2023-02-21 11:47:21 -0300

    Estilização da lista de tarefas

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { FaPlus } from 'react-icons/fa';
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Orar',
+      'Ler a Palavra',
+      'Estudar',
+    ],
   };
 
   handleChange = (e) => {
@@ -15,7 +21,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -29,6 +35,16 @@ export default class Main extends Component {
             <FaPlus />
             </button>
         </form>
+        <ul className='tarefas'>
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>{tarefa}
+              <div>
+                <FaEdit className='edit'/>
+                <FaWindowClose className='delete'/>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
